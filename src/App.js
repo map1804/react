5f80e4dfafcf1d85766ade271c6bdd3d22d2ee32
phuongnt <mapdethuong184@gmail.com>
2021-07-02 14:24:59 +0700

    props-this.props.key

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, {Component} from 'react';
-import './App.css';
-import Header from './components/header';
-import Product from './components/product';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/header";
+import Product from "./components/product";
 
 class App extends Component {
-  render(){
+  render() {
     return (
-        <div>
-          <Header></Header>
-          <div className="row">
-            <div className="col-lg-6">
-              <Product />
-            </div>
-          </div>
-        </div>
+      <div className="container-fluid">
+        <Header></Header>
+        <Product
+          name="Apple Iphone 6 plus"
+          price="16.000.000"
+          img="https://images.unsplash.com/photo-1575695342320-d2d2d2f9b73f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c21hcnQlMjBwaG9uZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+        />
+        <Product
+          name="Apple Iphone 7 plus"
+          price="17.000.000"
+          img="https://www.oppo.com/content/dam/oppo/common/mkt/v2-2/f17-pro/navigation/F17%20pro-navigation-blue-v2.png"
+        />
+        <Product
+          name="Apple Iphone 8 plus"
+          price="18.000.000"
+          img="https://images.unsplash.com/photo-1575695342320-d2d2d2f9b73f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c21hcnQlMjBwaG9uZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+        />
+      </div>
     );
   }
 }
